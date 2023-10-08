e5b9a53b2b6bc02cc9ae96cc17c06e36960cca27
upupnoah <upupqi.cs@gmail.com>
2023-10-09 02:57:13 +0800

    refactor(leetcode-daily): add array length calculation in problem 2578 solution
    This commit adds a line of code `n := len(nums)` to calculate the length of the array `nums` in the solution for problem 2578 - Split With Minimum Sum. This refactor improves readability and avoids recalculating the array length

--- LeetCode/2578. Split With Minimum Sum/main.go
+++ LeetCode/2578. Split With Minimum Sum/main.go
@@ -12,7 +12,8 @@ func splitNum(num int) int {
 	}
 	sort.Ints(nums)
 	num1, num2 := 0, 0
-	for i := 0; i < len(nums); i += 2 {
+	n := len(nums)
+	for i := 0; i < n; i += 2 {
 		num1 = num1*10 + nums[i]
 		if i+1 < n {
 			num2 = num2*10 + nums[i+1]
