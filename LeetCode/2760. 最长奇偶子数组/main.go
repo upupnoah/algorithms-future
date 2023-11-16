5cb62f3cb8c1be37a3180b849915b99db5f74a27
Noah <upupqi.cs@gmail.com>
2023-11-16 12:49:35 +0800

    feat(leetcode-daily): complete the daily leetcode problem

---
+++ LeetCode/2760. 最长奇偶子数组/main.go
@@ -0,0 +1,20 @@
+package main
+
+func longestAlternatingSubarray(nums []int, threshold int) (ans int) {
+	n := len(nums)
+	i := 0
+	for i < n {
+		if nums[i] > threshold || nums[i]%2 != 0 {
+			i++ // 直接跳过
+			continue
+		}
+		start := i // 记录这一组的开始位置
+		i++ // 开始位置已经满足要求，从下一个位置开始判断
+		for i < n && nums[i] <= threshold && nums[i]%2 != nums[i-1]%2 {
+			i++
+		}
+		// 从 start 到 i-1 是满足题目要求的子数组
+		ans = max(ans, i-start)
+	}
+	return ans
+}
