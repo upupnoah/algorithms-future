62b31a9a2febe1ff865fdc78433a79939cc0255e
Noah <upupqi.cs@gmail.com>
2024-01-03 16:35:07 +0800

    fix: delete unused mod

--- leetcode-rs/examples/noah.rs
+++ leetcode-rs/examples/noah.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 
-use std::ptr::eq;
-
 struct Solution;
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -20,8 +18,8 @@ impl Solution {
     pub fn remove_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         if let Some(mut node) = head {
             let next = node.next.take(); // take ownership of the next node, leaving None in its place
-            // assert_eq!(node.next, None);
-            
+                                         // assert_eq!(node.next, None);
+
             node.next = Self::remove_nodes(next);
             if let Some(next_node) = node.next.as_ref() {
                 if node.val < next_node.val {
