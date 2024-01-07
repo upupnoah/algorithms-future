960f23a317dbe7cc6f05e36a60ab1cdae82bfc06
Noah <upupqi.cs@gmail.com>
2024-01-07 15:28:13 +0800

    feat(leetcode-daily): 2024-01-07

--- leetcode-rs/examples/2807. Insert Greatest Common Divisors in Linked List.rs
+++ leetcode-rs/examples/2807. Insert Greatest Common Divisors in Linked List.rs
@@ -1,5 +1,38 @@
 #![allow(dead_code)]
 struct Solution;
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
 
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+impl Solution {
+    pub fn insert_greatest_common_divisors(
+        mut head: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut cur = &mut head;
+
+        while cur.as_ref().unwrap().next.is_some() {
+            let x = cur.as_mut().unwrap();
+            let next = x.next.take();
+            x.next = Some(Box::new(ListNode {
+                val: gcd(x.val, next.as_ref().unwrap().val),
+                next,
+            }));
+            cur = &mut cur.as_mut().unwrap().next.as_mut().unwrap().next;
+        }
+        head
+    }
+}
+fn gcd(a: i32, b: i32) -> i32 {
+    return if b == 0 { a } else { gcd(b, a % b) };
+}
 
 fn main() {}
