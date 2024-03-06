92d2416a4b77418570dc9558830c20b5d6c254db
Noah <upupqi.cs@gmail.com>
2024-03-06 09:59:38 +0800

    feat(leetcode-daily): 2024-03-06

---
+++ leetcode-go/2917. 找出数组中的 K-or 值/main.go
@@ -0,0 +1,14 @@
+package main
+
+func findKOr(nums []int, k int) (ans int) {
+	for i := 0; i < 31; i++ {
+		cnt1 := 0
+		for _, x := range nums {
+			cnt1 += x >> i & 1
+		}
+		if cnt1 >= k {
+			ans |= 1 << i
+		}
+	}
+	return ans
+}
