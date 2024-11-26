527c6741892579010c2f529199a91b3e5d360cb6
upupnoah <upupqi.cs@gmail.com>
2024-11-27 05:19:32 +0800

    feat(leetcode-daily): 2024-11-27

---
+++ leetcode-go/3208. 交替组 II/main.go
@@ -0,0 +1,16 @@
+package main
+
+func numberOfAlternatingGroups(colors []int, k int) (ans int) {
+	n := len(colors)
+	cnt := 0
+	for i := 0; i < n*2; i++ {
+		if i > 0 && colors[i%n] == colors[(i-1)%n] {
+			cnt = 0
+		}
+		cnt++
+		if i >= n && cnt >= k {
+			ans++
+		}
+	}
+	return
+}
